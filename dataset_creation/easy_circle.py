e81ad03f03c577f0317a8a48a58ee44dc8732e4b
tcotte <tristan.cotte@outlook.fr>
2023-05-23 11:10:03 +0200

    Add argparse for dataset creation

--- dataset_creation/easy_circle.py
+++ dataset_creation/easy_circle.py
@@ -4,11 +4,20 @@ import cv2
 import numpy as np
 from tqdm import tqdm
 
-img_height = 128
-img_width = 128
-nb_samples = 500
+import argparse
 
-path_dataset = r"C:\Users\tristan_cotte\PycharmProjects\CircleDetection\datasets\dataset_circle\val"
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--img_w', type=int, default=128, help='Image width')
+parser.add_argument('--img_h', type=int, default=128, help='Image height')
+parser.add_argument('--path', type=str, required=True, help='Dataset path')
+parser.add_argument('--nb_samples', type=int, required=True, help='Number of samples in the created dataset')
+
+args = parser.parse_args()
+
+img_height = args.img_h
+img_width = args.img_w
+nb_samples = args.nb_samples
+path_dataset = args.path
 
 if __name__ == "__main__":
     print("[INFO] Directories creation")
@@ -21,8 +30,8 @@ if __name__ == "__main__":
         image = np.zeros((img_height, img_width, 3), np.uint8)
 
         radius = np.random.randint(low=5, high=round(img_height / 3))
-        x_center_pos = np.random.randint(low=radius, high=img_width-2*radius)
-        y_center_pos = np.random.randint(low=radius, high=img_height-2*radius)
+        x_center_pos = np.random.randint(low=radius, high=img_width - 2 * radius)
+        y_center_pos = np.random.randint(low=radius, high=img_height - 2 * radius)
 
         color = tuple(np.random.randint(low=0, high=200, size=3, dtype=int))
         # convert to native Python types to be supported by OpenCV
@@ -32,11 +41,8 @@ if __name__ == "__main__":
 
         cv2.imwrite(os.path.join(path_dataset, "img", str(i) + '.png'), image)
 
-
         # xyxy
-        rect = [x_center_pos-radius, y_center_pos-radius, x_center_pos+radius, y_center_pos + radius]
+        rect = [x_center_pos - radius, y_center_pos - radius, x_center_pos + radius, y_center_pos + radius]
         rect = [str(x) for x in rect]
         with open(os.path.join(path_dataset, "labels", str(i) + '.txt'), 'w') as f:
             f.write(" ".join(rect))
-
-
