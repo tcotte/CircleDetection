Fix bboxes scaling

detect.py
@@ -1,6 +1,10 @@
+import os
 import time
 
+import cv2
+import numpy as np
 import torch
+from matplotlib import pyplot as plt
 
 from logger import WeightandBiaises
 from torch_datasets import CustomImageDataset
@@ -18,7 +22,7 @@ if __name__ == "__main__":
 
 
 
-    index = 18
+    index = 21
 
     start = time.time()
 
@@ -26,42 +30,42 @@ if __name__ == "__main__":
     model.eval()
 
 
-    for (images, labels, bboxes, filenames) in validation_loader:
-        # send the input to the device
-        labels = torch.Tensor(labels)
-        # bboxes = torch.stack(bboxes, dim=1)
+    # for (images, labels, bboxes, filenames) in validation_loader:
+    #     # send the input to the device
+    #     labels = torch.Tensor(labels)
+    #     # bboxes = torch.stack(bboxes, dim=1)
+    #
+    #     bboxes = bboxes.to(torch.float32)
+    #     bboxes = torch.squeeze(bboxes, 1)
+    #
+    #     predictions = model(images)
+    #
+    #     print(predictions)
+    #
+    #     logger = WeightandBiaises(interval_display=1)
+    #     logger.plot_one_batch(pred_batch=predictions[0], x_batch=images, y_batch=[None]*BATCH_SIZE, e=1)
+    #     break
 
-        bboxes = bboxes.to(torch.float32)
-        bboxes = torch.squeeze(bboxes, 1)
 
-        predictions = model(images)
+    with torch.no_grad():
+        # print(train_dataset[0][0])
+        image = val_dataset[index][0].type(torch.FloatTensor)
 
-        print(predictions)
+        image = torch.unsqueeze(image, 0)
 
-        logger = WeightandBiaises(interval_display=1)
-        logger.plot_one_batch(pred_batch=predictions[0], x_batch=images, y_batch=[None]*BATCH_SIZE, e=1)
-        break
+        output = model(image)[0]
+        coords = 128*output[0]
 
 
-    # with torch.no_grad():
-    #     # print(train_dataset[0][0])
-    #     image = val_dataset[index][0].type(torch.FloatTensor)
-    #
-    #     image = torch.unsqueeze(image, 0)
-    #
-    #     output = model(image)[0]
-    #     coords = 128*output[0]
-    #
-    #
-    #     print(get_iou(np.array(128*train_dataset[index][2]), coords.numpy()))
-    #
-    #     coords = np.round(coords.numpy())
-    #     coords = [int(x) for x in coords]
-    #
-    #
-    #     image = cv2.imread(os.path.join(r"datasets/dataset_circle/val/img", val_dataset[index][-1]))
-    #     cv2.rectangle(image, (coords[0], coords[1]), (coords[2], coords[3]), (0, 255, 255), 1)
-    #     plt.imshow(image)
-    #     plt.show()
-    #
-    # print(time.time()-start)
+        # print(get_iou(np.array(128*train_dataset[index][2]), coords.numpy()))
+
+        coords = np.round(coords.numpy())
+        coords = [int(x) for x in coords]
+
+
+        image = cv2.imread(os.path.join(r"datasets/dataset_circle/val/img", val_dataset[index][-1]))
+        cv2.rectangle(image, (coords[0], coords[1]), (coords[2], coords[3]), (0, 255, 255), 1)
+        plt.imshow(image)
+        plt.show()
+
+    print(time.time()-start)

torch_datasets.py
@@ -4,9 +4,6 @@ import cv2
 import numpy as np
 import torch
 from torch.utils.data import Dataset
-from torchvision.io import read_image
-
-IMG_SIZE = 128
 
 
 class CustomImageDataset(Dataset):
@@ -33,7 +30,16 @@ class CustomImageDataset(Dataset):
         bboxes = txtfile.readline().split(" ")
         txtfile.close()
 
-        bboxes = np.array([[float(x) / IMG_SIZE for x in bboxes]])
+        # bboxes = np.array([[float(x) / IMG_SIZE for x in bboxes]])
+        bboxes = np.array(bboxes)
+        if bboxes.ndim == 1:
+            bboxes = np.expand_dims(bboxes, axis=0)
+
+        img_h, img_w = image.shape[:2]
+        bboxes[:, 0] = bboxes[:, 0] / img_w
+        bboxes[:, 1] = bboxes[:, 1] / img_h
+        bboxes[:, 2] = bboxes[:, 2] / img_w
+        bboxes[:, 3] = bboxes[:, 3] / img_h
         category_ids = np.zeros(len(bboxes), dtype=int)
 
         if self.transform: